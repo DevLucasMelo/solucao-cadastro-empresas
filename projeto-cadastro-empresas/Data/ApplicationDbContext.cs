a88ae8eee907fd9781b6ff13dc1df336f6b44368
Lucas Faustini de Melo <faustinilucas8@gmail.com>
2025-07-29 08:32:03 -0300

    feat: Adicionado as ApplicationsDbContext e ApplicationDbContextFacotry

---
+++ projeto-cadastro-empresas/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CadastroEmpresasApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CadastroEmpresasApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Empresa> Empresas { get; set; }
+    }
+}
