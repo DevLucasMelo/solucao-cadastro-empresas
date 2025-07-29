ff8274d0d5601d224ade63f19b6f329cb1aeef11
Lucas Faustini de Melo <faustinilucas8@gmail.com>
2025-07-29 08:31:25 -0300

    feat: Adicionado os modelos Usuario e Empresa

---
+++ projeto-cadastro-empresas/Models/Empresa.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CadastroEmpresasApp.Models
+{
+    public class Empresa
+    {
+        [Key]
+        public int emp_id { get; set; }
+
+        [Required]
+        public string emp_cnpj { get; set; }
+
+        public string emp_nome_empresarial { get; set; }
+        public string emp_nome_fantasia { get; set; }
+        public string emp_situacao { get; set; }
+        public string emp_abertura { get; set; }
+        public string emp_tipo { get; set; }
+        public string emp_natureza_juridica { get; set; }
+        public string emp_atividade_principal { get; set; }
+
+        public string emp_logradouro { get; set; }
+        public string emp_numero { get; set; }
+        public string emp_complemento { get; set; }
+        public string emp_bairro { get; set; }
+        public string emp_municipio { get; set; }
+        public string emp_uf { get; set; }
+        public string emp_cep { get; set; }
+
+        [ForeignKey("Usuario")]
+        public int emp_usu_id { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+}
---
+++ projeto-cadastro-empresas/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadastroEmpresasApp.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int usu_id { get; set; }
+
+        [Required]
+        public string usu_nome { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string usu_email { get; set; }
+
+        [Required]
+        public string usu_senha_hash { get; set; }
+    }
+}
